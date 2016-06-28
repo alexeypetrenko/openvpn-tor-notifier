2d7282726c7790a6889f22b953e2c84bbc1e189e
Alexey Petrenko <mr.alexey.petrenko@gmail.com>
2016-06-28 18:49:12 +0500

    Gather TOR exit nodes addresses from multiple sources

--- update-tor-exit-nodes.sh
+++ update-tor-exit-nodes.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 TOR_EXIT_NODES_FILE="/tmp/tor-exit.lst"
-curl -sSL https://check.torproject.org/exit-addresses | grep -F ExitAddress | awk '{print $2}' | sort -u > $TOR_EXIT_NODES_FILE
+SERVER_IP=8.8.8.8
+SERVER_PORT=443
+curl -sSL https://check.torproject.org/exit-addresses | grep -F ExitAddress | awk '{print $2}' > "${TOR_EXIT_NODES_FILE}.raw"
+curl -sSL "https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=${SERVER_IP}&port=${SERVER_PORT}" | grep -vE '^\s*#' >> "${TOR_EXIT_NODES_FILE}.raw"
+
+sort -u < "${TOR_EXIT_NODES_FILE}.raw" > ${TOR_EXIT_NODES_FILE}
+rm "${TOR_EXIT_NODES_FILE}.raw"
